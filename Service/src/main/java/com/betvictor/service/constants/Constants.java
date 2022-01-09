97ac724e8ce9bb627ec372b620460dc42e7e5ba9
Botond <barabas.botond@webtown.hu>
2022-01-09 17:35:58 +0100

    service: comment repo

---
+++ Service/src/main/java/com/betvictor/service/constants/Constants.java
@@ -0,0 +1,9 @@
+package com.betvictor.service.constants;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public final class Constants {
+
+  public static final String ORDER_DESTINATION_NAME = "comment-destination-name";
+}
